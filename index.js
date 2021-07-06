2cd460184d88f47b484ffc44c4b051e3baecc4ba
Imagine Rabbits <keerati.jear@gmail.com>
2021-07-06 19:58:32 +0700

    add sample metrics

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
 const promClient = require('prom-client')
 const collectDefaultMetrics = promClient.collectDefaultMetrics
-const Registry = promClient.Registry
-const register = new Registry()
-collectDefaultMetrics({ register })
+const registry = new promClient.Registry()
 
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.listen(port, () => {
+    collectDefaultMetrics({ register: registry })
+
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
 app.get('/metrics', async (req, res) => {
-    res.send(await register.metrics())
+    res.send(await registry.metrics())
 })
 
 const counter = new promClient.Counter({
-    name: 'counter',
-    help: 'counter_help',
+    name: 'my_counter',
+    help: 'my_counter_help',
 });
+registry.registerMetric(counter)
 
 const gauge = new promClient.Gauge({
-    name: 'gauge',
-    help: 'gauge_help',
+    name: 'my_gauge',
+    help: 'my_gauge_help',
 });
+registry.registerMetric(gauge)
 
 const histrogram = new promClient.Histogram({
-    name: 'histrogram',
-    help: 'histrogram_help',
+    name: 'my_histrogram',
+    help: 'my_histrogram_help',
 });
+registry.registerMetric(histrogram)
 
 app.get('/', (req, res) => {
     counter.inc();
     gauge.inc();
-    histrogram.observe();
+    histrogram.observe(10);
 
     res.send('Hello World!')
 })
